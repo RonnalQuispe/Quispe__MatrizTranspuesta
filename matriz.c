d25435492d9f9b4eeee3e569195291af968ffa2a
Laboratorio <laboratorio@udla-ec.int>
2023-11-27 16:21:48 -0500

    agregamos una libreria stdlib.h para que funcione la funcion RAND

--- matriz.c
+++ matriz.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main()
 {
-    int filas, columnas;
-    ;
-    // Obtener dimensiones de la matriz
+    int fila, columna;
+        // Obtener dimensiones de la matriz
     printf("Ingresa el numero de filas: ");
-    scanf("%d", &filas);
+    scanf("%d", &fila);
     printf("Ingresa el numero de columnas: ");
-    scanf("%d", &columnas);
+    scanf("%d", &columna);
     // Crear matriz original con números aleatorios entre 0 y 100
-    int matriz_original[filas][columnas]
+    int matriz_original[fila][columna];
     for (int i = 0; i < filas; i++)
     {
         for (int j = 0; j < columnas; j++)
@@ -20,13 +19,15 @@ int main()
     }
      // Imprimir matriz original
     printf("Matriz original:\n");
-    for(int i = 0; i < filas; i++) {
-        for(int j = 0; j < columnas; j++) {
+    for(int i = 0; i < fila; i++) {
+        for(int j = 0; j < columna; j++) {
             printf("%d ", matriz_original[i][j]);
         }
         printf("\n");
     }
     
-
+    
+    
     return 0;
 }
+
