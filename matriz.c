2b6e8b411d374000632b927824e8a4fe4ec4b67e
Laboratorio <laboratorio@udla-ec.int>
2023-11-27 16:02:54 -0500

    proyecta ingresar numeros y filas de la matriz

---
+++ matriz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int filas, columnas;
+
+    // Obtener dimensiones de la matriz
+    printf("Ingresa el numero de filas: ");
+    scanf("%d", &filas);
+    printf("Ingresa el numero de columnas: ");
+    scanf("%d", &columnas);
+
+    return 0;
+}
