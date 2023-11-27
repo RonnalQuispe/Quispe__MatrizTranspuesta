633e28174926e418e3652370c4471ae9ffddbea4
Laboratorio <laboratorio@udla-ec.int>
2023-11-27 16:07:26 -0500

    con el numeor de F y C que se asignaron crear y asgnar numeros entre 0-100

--- matriz.c
+++ matriz.c
@@ -3,12 +3,21 @@
 int main()
 {
     int filas, columnas;
-
+    ;
     // Obtener dimensiones de la matriz
     printf("Ingresa el numero de filas: ");
     scanf("%d", &filas);
     printf("Ingresa el numero de columnas: ");
     scanf("%d", &columnas);
+    // Crear matriz original con números aleatorios entre 0 y 100
+    int matriz_original[filas][columnas]
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            matriz_original[i][j] = rand() % 101;//asignar numeros enteros entre 0 y 100
+        }
+    }
 
     return 0;
 }
